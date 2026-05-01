5e6ebba8708a5699d39fedb73e3bf4cba9e0dc91
shivaDebugs <shiva12300@gmail.com>
2026-05-01 08:27:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ this file contains the deployment script..
 added this script..
 adding this line in master branch
 adding this line in development branch
+
+updated in uat branch...directly in remote repo..
